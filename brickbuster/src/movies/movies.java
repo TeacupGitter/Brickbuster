85f9571baf1c45bc6c9c2b69fd21efa33868cdc2
TeacupGitter <maren.r.paulsen@gmail.com>
2024-11-21 14:28:02 +0000

    new file:   brickbuster/src/movies/movies.java

---
+++ brickbuster/src/movies/movies.java
@@ -0,0 +1,17 @@
+package movies;
+
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+public class movies {
+    public static void main(String[] args) {
+        JFrame frame = new JFrame();
+        frame.setSize(960,720);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+
+        JPanel panel = new JPanel();
+    }
+
+}
